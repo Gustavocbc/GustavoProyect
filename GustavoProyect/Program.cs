175637fc0f46803fd79e81f0bc39c94fbc8b0210
Gustavocbc <ceballosgustavocar@coruniamericana.edu.co>
2015-06-15 21:38:57 -0500

    Correccion Tab Lineas

--- GustavoProyect/Program.cs
+++ GustavoProyect/Program.cs
@@ -10,7 +10,25 @@ namespace GustavoProyect
     {
         static void Main(string[] args)
         {
-
+            int a = 0, i, n;
+            Console.WriteLine("Ingrese numero");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < (n + 1); i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a != 2)
+            {
+                Console.WriteLine("No es Primo");
+            }
+            else
+            {
+                Console.WriteLine("Si es Primo");
+            }
+            Console.ReadLine(); 
         }
     }
 }
